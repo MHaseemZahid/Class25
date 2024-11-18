aecd140e98f3c4bd0eeef0b55d51c5c04a0b7e2d
MHaseemZahid <haseem.zahid@gmail.com>
2024-11-18 22:50:22 +0500

    how to perform maths in alert and line brake in string

--- class25/app.js
+++ class25/app.js
@@ -1,1 +1,34 @@
-alert('hi')
+// alert('hi');
+// alert(10+10);
+
+// data types
+
+// String all charters exp numbers
+// Numbers  all numbers 1234567890
+// all string written in '',"",``
+
+// var quor1 ="a quick brown fox jumps over the lazy dog";
+
+// var msg1 ="as salamu alaikum ";
+// var msg2 =" Haseem";
+
+// var v1 =2;
+// var v2 =2;
+// var v3 =8;
+// var v4 =2;
+// var v5 =2;
+// var v6 =2;
+// var v7 =2;
+// var v8 =2;
+
+// alert(msg1+msg2);
+
+// alert(v1+v2);
+
+// alert(v3-v4);
+
+// alert(v5*v6);
+
+// alert(v7/v8);
+
+alert("a quick brown fox jumps \n over the lazy dog");
